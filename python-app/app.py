0215fbf2e7d2346cacd87e7fce67132a86e5ea00
Wojciech Kuchta <crabbulla@gmail.com>
2022-05-21 17:35:44 +0200

    python project

--- python-app/app.py
+++ python-app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
 import os
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, App From Docker!'
+    return 'Hello, this our python app from Docker!'
